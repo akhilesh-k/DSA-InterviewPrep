7a5baa878e995e0b2b7e199169fd38fe1ef6aaaa
Akhilesh <akhilesh_k@outlook.com>
2019-11-14 14:54:35 +0530

    adding the solution in more clearer format.

--- Interview_Preparation_Kit/Warm_Up_Challenges/Jumping_on_clouds.cpp
+++ Interview_Preparation_Kit/Warm_Up_Challenges/Jumping_on_clouds.cpp
@@ -6,7 +6,15 @@ int main(){
     for(int i=0;i<=n;i++)
     cin>>arr[i];
     for(int i=0;i<(n-1);) 
-    arr[i+2]?(res++,i++):(res++,i+=2); // Simple approach, it checks all the (i+2)th element and         
+    //arr[i+2]?(res++,i++):(res++,i+=2); // Simple approach, it checks all the (i+2)th element and         
+    if(arr[i+2] != 0){
+	res++;
+    i++;
+}
+else{
+	res++;
+    i += 2;
+}
     cout<<res;
     return 0;
 }
