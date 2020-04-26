1d2a2032ec96b4034ff0d5799bfcc951a93fa6ff
Akhilesh <akhilesh_k@outlook.com>
2020-04-26 09:58:05 +0530

    updated bubble sort

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -20,16 +20,17 @@ void bubbleSort(int *arr, int size)
 {
     for (int i = 0; i < size; i++)
     {
-        int count = 0;
+        int flag = 0;
         for (int j = 0; j < size - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swapnum(arr[j], arr[j + 1]);
-                count = 1;
+                flag = 1;
             }
         }
-        if (!count)
+        // Breaking the loop on swap completion
+        if (!flag)
             break;
     }
 }
